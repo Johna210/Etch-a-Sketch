954078136f03555f6f1c8ea5fbdd152d9e10772c
Johna210 <solomonjohna21@gmail.com>
2024-01-19 12:07:31 +0300

    removed console logs

--- main.js
+++ main.js
@@ -61,7 +61,6 @@ function colorFunction(color) {
                 e.target.style.backgroundColor = `rgb(${colors[0]},${colors[1]},${colors[2]})`;
             } else {
                 e.target.style.backgroundColor = color;
-                console.log(e.target);
             }
         }
     });
@@ -76,7 +75,6 @@ function colorFunction(color) {
                     e.target.style.backgroundColor = `rgb(${colors[0]},${colors[1]},${colors[2]})`;
                 } else {
                     e.target.style.backgroundColor = color;
-                    console.log(e.target);
                 }
             }
         }
